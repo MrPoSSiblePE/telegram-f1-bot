4a43aff733c017c6e52603b88b8b3773bcea2cbc
Tuomas Änäkkälä <tuomas.anakkala@icloud.com>
2017-04-15 18:11:06 +0300

    use .env file when not in production environment

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var env = require('node-env-file');
-// env('.env');
-env(__dirname + '/.env');
+if(environment !== "production") {
+  env(__dirname + '/.env');
+}
 var bot = require('./app/bot.js');
 require('./app/webapp.js')(bot);
 require('./app/functions.js')(bot);
